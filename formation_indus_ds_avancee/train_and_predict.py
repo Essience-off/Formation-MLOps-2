2cc1c7d7cfa33cf39dce83798da2b9117de54f02
Essience <tancredehayer@orange.fr>
2024-10-09 07:34:16 +0000

    tp6

--- formation_indus_ds_avancee/train_and_predict.py
+++ formation_indus_ds_avancee/train_and_predict.py
@@ -19,7 +19,8 @@ def train_model(features: pd.DataFrame, model_registry_folder: str) -> None:
     y = features[target]
     with mlflow.start_run():
         # insert autolog here ...
-        model = RandomForestRegressor(n_estimators=1, max_depth=10, n_jobs=1)
+        mlflow.sklearn.autolog()
+        model = RandomForestRegressor(n_estimators=4, max_depth=7, n_jobs=1)
         model.fit(X, y)
     time_str = time.strftime('%Y%m%d-%H%M%S')
     joblib.dump(model, os.path.join(model_registry_folder, time_str + '.joblib'))
