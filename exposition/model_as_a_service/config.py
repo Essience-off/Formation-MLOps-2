e2d8b58b31cb12e26ae6fff1a25440b30e34756c
Essience <tancredehayer@orange.fr>
2024-10-09 08:13:44 +0000

    tp7 pred in flask

--- exposition/model_as_a_service/config.py
+++ exposition/model_as_a_service/config.py
@@ -12,7 +12,7 @@ TRAIN_DATA_PATH = os.path.join(DATA_FOLDER, 'la-haute-borne-data-2017-2020.csv')
 GENERATED_DATA_PATH = os.path.join(DATA_FOLDER,
                                    'la-haute-borne-data-2017-2020.csv')  # TODO change, should point to generator
 
-FEATURES_PATH = os.path.join(DATA_FOLDER, 'prepared_features.parquet')
+FEATURES_PATH = os.path.join(DATA_FOLDER, 'prepared_features_train.parquet')
 
 MODEL_REGISTRY_FOLDER = '.'
 MODEL_PATH = os.path.join(MODEL_REGISTRY_FOLDER,
